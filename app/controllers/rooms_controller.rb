10f8531112e807bd6ff6938f9e848088e311eca3
Murakami0548 <94536915+Murakami0548@users.noreply.github.com>
2021-12-14 23:20:14 +0900

    データ紛失により新規登録

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,19 +1,31 @@
-def new
-  @room = Room.new
-end
+class RoomsController < ApplicationController
 
-def create
-  @room = Room.new(room_params)
-  if @room.save
-    redirect_to root_path
-  else
-    render :new
+  def index
+
   end
-end
+
+  def new
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    room = Room.find(params[:id])
+    room.destroy
+    redirect_to root_path
+  end
 
   private
 
   def room_params
-    params.require(:room).permit(:name, user_ids: [])
+    params.require(:room).permit(:name, user_ids:[])
   end
 end
